9f47b40279201b0b0daec2518640b889d1aa1ec3
Michael Loughman <mcloughman@gmail.com>
2023-03-13 18:57:57 -0700

    The ninja explains how to use our library in a project. We import our shinobi directory into whatever project folder. And  we will need to create a gulp file. Easy enough. BUT he also shows us how we might make changes or tweaks to our library. We create a sep arate sass folder and make index.scss within it. We change gulpfile to now watch and compile from this sass folder. In the new index file,  we can change the css rules that we want. BUT, WE NEED TO ADD default within relevant shinobi file and rules. This basically tells the r ule to use this as a default value UNLESS another is specified. We make our changes above before importing shinobi. Video 21 in the series . May need to watch again

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ const purgecss = require("gulp-purgecss");
 // this function will comppile our scss from shinobi and it's subdirectories and pipe it into css and pipe it into our css folder. we use the asterisk because we may have multiple scss files
 function buildStyles() {
   return (
-    src("shinobi/**/*.scss")
+    src("sass/**/*.scss")
       .pipe(sass())
       // purgecss will look at our html files in the root directory to see what css is required. Then it will purge the unused css rules from the index.css
       .pipe(purgecss({ content: ["*.html"] }))
@@ -15,7 +15,7 @@ function buildStyles() {
 
 // this function will watch our sass file and update accordingly. And now since we added purgecss, we will have it watch out html files for updates as well
 function watchTask() {
-  watch(["shinobi/**/*.scss", "*.html"], buildStyles);
+  watch(["sass/**/*.scss", "*.html"], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
