b549d89941efd8a6253a507c04e96dffbd4d7aba
Michael Loughman <mcloughman@gmail.com>
2023-03-13 18:11:14 -0700

    add gulp-purgecss to rid the index.css file of unused rules. when we do want to add classes to our html, we update the watchTask function in the gulp.js file to watch our html files for changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,21 @@
 const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const purgecss = require("gulp-purgecss");
 
 // this function will comppile our scss from shinobi and it's subdirectories and pipe it into css and pipe it into our css folder. we use the asterisk because we may have multiple scss files
 function buildStyles() {
-  return src("shinobi/**/*.scss").pipe(sass()).pipe(dest("css"));
+  return (
+    src("shinobi/**/*.scss")
+      .pipe(sass())
+      // purgecss will look at our html files in the root directory to see what css is required. Then it will purge the unused css rules from the index.css
+      .pipe(purgecss({ content: ["*.html"] }))
+      .pipe(dest("css"))
+  );
 }
 
-// this function will watch our sass file and update accordingly
+// this function will watch our sass file and update accordingly. And now since we added purgecss, we will have it watch out html files for updates as well
 function watchTask() {
-  watch(["shinobi/**/*.scss"], buildStyles);
+  watch(["shinobi/**/*.scss", "*.html"], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
